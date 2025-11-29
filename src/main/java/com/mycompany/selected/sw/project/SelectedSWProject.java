027806e20c133b9562466b7468a01d6b864fdffc
Ahmed Youssef <ahmed.youssef.5522.52@gmail.com>
2025-11-29 18:02:20 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/selected/sw/project/SelectedSWProject.java
@@ -0,0 +1,7 @@
+package com.mycompany.selected.sw.project;
+public class SelectedSWProject {
+
+    public static void main(String[] args) {
+        System.out.println("Initialising Git, GitHub");
+    }
+}
