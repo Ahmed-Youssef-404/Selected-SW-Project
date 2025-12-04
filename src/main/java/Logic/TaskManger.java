25ec70cdc4d4a6f972ea213665f82f659c8d62ad
Ahmed Youssef <ahmed.youssef.5522.52@gmail.com>
2025-12-04 16:12:22 +0200

    feat(printed observers notifications in the tirmenal

--- src/main/java/Logic/TaskManger.java
+++ src/main/java/Logic/TaskManger.java
@@ -28,17 +28,16 @@ public class TaskManger {
         tasks.add(t);
         notifyObservers("New task '" + t.getTitle() + "' created and assigned to " + (t.getAssignedTo() != null ? t.getAssignedTo() : "No one"));
     }
-    
+
 //    public void completeTask(Task t){
 //        t.setStatus("Completed");
 //        notifyObservers("Task '" + t.getTitle() + "' has been marked as Completed by " + t.getAssignedTo());
 //    }
-
     public void addUser(User u) {
         users.add(u);
         observers.add(u);
     }
-    
+
     public List<Task> getTasks() {
         return tasks;
     }
@@ -46,11 +45,14 @@ public class TaskManger {
     public List<User> getUsers() {
         return users;
     }
-    
-    private void notifyObservers(String message){
-        for(TaskObserver observer: observers){
+
+    private void notifyObservers(String message) {
+        for (TaskObserver observer : observers) {
             observer.update(message);
         }
 //        NotificationsService.getInstance().sendNotification(message);
+        NotificationsService.getInstance().showNotification(message, text -> {
+            System.out.println(text);;
+        });
     }
 }
